8004f68a28f15819e9dbbc8955ee1bf7e3173902
Colin Wong <cwongmath@gmail.com>
2023-10-18 21:50:40 -0500

    Fix warnings and #2 server crash

--- src/main/java/com/omicron/spectral_edge/ModEnchantments.java
+++ src/main/java/com/omicron/spectral_edge/ModEnchantments.java
@@ -1,9 +1,7 @@
 package com.omicron.spectral_edge;
 
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -22,13 +20,5 @@ public class ModEnchantments {
     public static void registerEnchantments(RegistryEvent.Register<Enchantment> event)
     {
         event.getRegistry().register(SPECTRAL_EDGE);
-        //System.out.println("enchantment_test");
-    }
-
-    @SubscribeEvent
-    public static void onTooltip(ItemTooltipEvent event)
-    {
-        //System.out.println(event.isCancelable());
-
     }
 }
